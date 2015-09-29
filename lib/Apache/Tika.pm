b9f1e1a77dd22b14641f148ccdd8941ac31e93c3
Gerard Ribugent <ribugent@gmail.com>
2015-09-30 00:09:20 +0200

    Created constructor

--- lib/Apache/Tika.pm
+++ lib/Apache/Tika.pm
@@ -1,5 +1,18 @@
 use strict;
 use warnings;
+
 package Apache::Tika;
+
+use LWP::UserAgent;
+
+sub new {
+	my ($this, %pars) = @_;
+
+	my $self = bless {}, $this;
+	$self->{ua} = $pars{ua} // LWP::UserAgent->new();
+	$self->{url} = $pars{url} // 'http://localhost:9998';
+
+	return $self;
+}
 
 1;
