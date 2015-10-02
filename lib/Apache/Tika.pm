41b494248a06ce1305ac66a926113d86ca4b7ebb
Gerard Ribugent <ribugent@gmail.com>
2015-10-03 00:11:16 +0200

    Add _request method Tika method

--- lib/Apache/Tika.pm
+++ lib/Apache/Tika.pm
@@ -15,4 +15,33 @@ sub new {
 	return $self;
 }
 
+sub _request {
+	my ($self, $method, $path, $headers, $bodyBytes) = @_;
+
+	# Perform request
+	my $response = $self->{ua}->$method(
+		$self->{url} . '/' . $path,
+		%$headers,
+		Content => $bodyBytes
+	);
+
+	# Check for errors
+	# TODO
+
+	return $response->decoded_content();
+}
+
+sub tika {
+	my ($self, $bytes, $contentType) = @_;
+	return $self->_request(
+		'put',
+		'tika',
+		{
+			'Accept-content' => 'plain/text',
+			$contentType? ('Content-type' => $contentType) : ()
+		},
+		$bytes
+	);
+}
+
 1;
